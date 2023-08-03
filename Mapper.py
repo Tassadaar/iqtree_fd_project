4a3caa3069ceae5840f3bed576ba49814f32142b
Jason Shao <Jason.Shao@dal.ca>
2023-08-03 11:35:53 -0300

    added commandline arguments parsing to Mapper.py

--- Mapper.py
+++ Mapper.py
@@ -1,5 +1,6 @@
-import copy
 import sys
+import copy
+import argparse
 from ete3 import Tree, TreeNode
 
 
@@ -37,22 +38,17 @@ def fix_topology(input_tree, result_tree):
     return new_tree
 
 
-def main():
+def main(args):
 
     try:
-        master_tree = Tree("((1:0.496516432,((21:0.2,(23:0.4,24:0.22):0.3):0.2750396589,3:0.4785155756):0.14105667032):0.1,"
-                       "(((4:0.2864272562,5:0.2870235802):0.2266968989,6:0.5159850109):0.1365803773,"
-                       "((7:0.7543316696,8:0.9338300134):0.34184149945,(9:0.2355059988,10:0.2518571253)"
-                       ":0.3972088265):0.4108541642):0.01);")
+        master_tree = Tree(args.master_tree)
         assert len(master_tree.get_children()) == 2, "Master tree must be rooted!"
     except AssertionError as e:
         print(f"Oops! {e}")
         sys.exit()
 
-    a_tree = Tree("(1:0.6121762192,(21:0.1881984527,(23:0.4160042306,24:0.2186291776):0.3451463477)"
-                      ":0.2916847347,3:0.5092916847);")
-    b_tree = Tree("(4:0.2681573187,5:0.2859668232,(6:0.4982657718,((7:0.7059120269,8:0.8755530768):0.3136818250,"
-                      "(9:0.2271793309,10:0.2279696348):0.4035351373):0.5616725078):0.2312659556);")
+    a_tree = Tree(args.a_tree)
+    b_tree = Tree(args.b_tree)
     sub_a_tree = master_tree.get_children()[0]
     sub_b_tree = master_tree.get_children()[1]
 
@@ -64,7 +60,23 @@ def main():
 
     print(new_master_tree.robinson_foulds(master_tree)[0])
     print(new_master_tree)
-    
+
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description="Tree mapper")
+
+    parser.add_argument("-m", "--master_tree", required=True)
+    parser.add_argument("-a", "--a_tree", required=True)
+    parser.add_argument("-b", "--b_tree", required=True)
+
+    # emulating commandline arguments for development
+    sys.argv = [
+        "Mapper.py",
+        "-m", "master.tree",
+        "-a", "a.tree",
+        "-b", "b.tree"
+
+    ]
+
+    arguments = parser.parse_args()
+    main(arguments)
